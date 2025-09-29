6d1a60f8cb85ae841b351c83bfeb109935322960
Alex08080 <alexmoderan@gmail.com>
2025-09-29 17:51:51 +0200

    part1

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,59 @@
+import { use } from 'react'
+import { useState } from 'react'
+
+
+const Button =({onClick, text})=>{
+    return(
+      <button onClick={onClick}>{text}</button>
+    )
+   }
+
+   const DisplayVote=({value})=>{
+    return(
+      <p>has {value} votes</p>
+    )
+   }
+   const DisplayAnecdote=({text})=>{
+    return(
+      <p>{text}</p>
+    )
+   }
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+const [selected, setSelected] = useState(0)
+const [votes, setVotes] = useState([0,0,0,0,0,0,0,0])
+const nextClick=()=>{
+  setSelected(Math.floor(Math.random()*(8)))
+}
+const plusvote=()=>{
+  const copy=[...votes]
+  copy[selected]+=1
+  setVotes(copy)
+}
+const max=Math.max(...votes)
+  return (
+    
+    <div>
+      <h1>Anecdote of the day</h1>
+      <DisplayAnecdote text={anecdotes[selected]}/>
+      <DisplayVote value={votes[selected]}/>
+      <Button onClick={plusvote} text='vote'/>
+      <Button onClick={nextClick} text='next anecdote'/>
+      <h1>Anecdote with most votes</h1>
+      <DisplayAnecdote text={anecdotes[votes.indexOf(max)]}/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text})=>{
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
+const StatisticLine=({text,value})=>{
+  return(
+    <tr>
+    <td>{text}</td> <td>{value}</td>
+    </tr>
+  )
+}
+const Statistics=({good,neutral,bad})=>{
+  if(good===0 && neutral===0 && bad===0){
+    return(
+  <div>No feedback given</div>
+  )}
+  const all=good+neutral+bad
+  const average = (good - bad )/ all
+  const positive = ((good)/all)*100
+  return(
+    <div>
+      <table>
+        <tbody>
+        <StatisticLine text="good" value ={good} />
+        <StatisticLine text="neutral" value ={neutral} />
+        <StatisticLine text="bad" value ={bad} />
+        <StatisticLine text="all" value={all}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={positive + "%"}/>
+        </tbody>
+      </table>
+  
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick=()=>{
+    setGood(good+1)
+  }
+  const badClick=()=>{
+    setBad(bad+1)
+  }
+  const neutralClick=()=>
+  {
+    setNeutral(neutral+1)
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={goodClick} text='good'/>
+      <Button onClick={neutralClick} text='neutral'/>
+      <Button onClick={badClick} text='bad'/>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
