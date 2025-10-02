52c1572439d9c38b7a2100f747c81417718d623f
Alex08080 <alexmoderan@gmail.com>
2025-10-02 16:51:42 +0200

    part3 en cours

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 app.use(cors())
 var morgan = require('morgan')
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('body',function(req,res){ return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 let persons = [
